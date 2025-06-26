a5cd5e0ebe3ad8c8002f83c900578e8629e5808a
jgtolentino <jgtolentino_rn@yahoo.com>
2025-06-26 09:29:24 +0800

    fix(deps): add tailwindcss, postcss & autoprefixer dependencies

--- postcss.config.js
+++ postcss.config.js
@@ -2,8 +2,5 @@ import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 
 export default {
-  plugins: [
-    tailwindcss('./tailwind.config.js'),
-    autoprefixer(),
-  ]
+  plugins: [tailwindcss, autoprefixer],
 };
